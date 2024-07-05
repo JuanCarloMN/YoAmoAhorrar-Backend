8f4b0a91d4246d78ba12f3dc032706202c00547a
Juan Carlo Morales <JCpunky@gmail.com>
2024-07-05 16:56:18 -0600

    Funcionalidad de catalogos

--- controllers/catalogos.js
+++ controllers/catalogos.js
@@ -15,7 +15,7 @@ const obtenerCatalogos = async ( req, res = response ) => {
         console.log( error );
         res.status(500).json({
             ok: false,
-            msg: 'Problemas al obtener los catalogos'
+            msg: 'Problemas al obtener los catálogos'
         })
     }
 }
@@ -36,7 +36,7 @@ const agregarCatalogo = async ( req, res = response ) => {
         console.log( error );
         res.status(500).json({
             ok: false,
-            msg: 'Problemas al agregar al catalogo'
+            msg: 'Problemas al agregar el catálogo'
         })
     }
 }
@@ -53,7 +53,7 @@ const actualizarCatalogo = async ( req, res = response ) => {
         if ( !catalogo ) {
             return res.status(404).json({
                 ok: false,
-                msg: 'No se encontró el catalogo para actualizar'
+                msg: 'No se encontró el catálogo para actualizar'
             });
         }
 
@@ -71,7 +71,7 @@ const actualizarCatalogo = async ( req, res = response ) => {
         console.log( error );
         res.status(500).json({
             ok: false,
-            msg: 'Problemas al actualizar la información del catalogo'
+            msg: 'Problemas al actualizar la información del catálogo'
         })
     }
 }
@@ -87,7 +87,7 @@ const eliminarCatalogo = async ( req, res = response ) => {
         if ( !catalogo ) {
             return res.status(404).json({
                 ok: false,
-                msg: 'No se encontró el catalogo para eliminar'
+                msg: 'No se encontró el catálogo para eliminar'
             });
         }
  
@@ -101,14 +101,125 @@ const eliminarCatalogo = async ( req, res = response ) => {
         console.log( error );
         res.status(500).json({
             ok: false,
-            msg: 'Problemas al eliminar al catalogo'
+            msg: 'Problemas al eliminar el catálogo'
+        })
+    }
+}
+
+const agregarDato = async ( req, res = response ) => {
+    const dato = req.body ;
+
+    try {
+        const catalogo = await Catalogo.findById( dato.id );
+        if ( !catalogo ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No se encontró el catálogo para actualizar'
+            });
+        }
+
+        const datoGuardado = await Catalogo.findOneAndUpdate( { _id: dato.id }, {
+            $push:{
+                catalogoDatos: { descripcion: dato.descripcion }
+            }
+        }, { new: true});
+
+        res.status(201).json({
+            ok: true,
+            dato: datoGuardado
+        })
+        
+    } catch (error) {
+        console.log( error );
+        res.status(500).json({
+            ok: false,
+            msg: 'Problemas al agregar el dato en el catálogo'
+        })
+    }
+}
+
+const actualizarDato = async ( req, res = response ) => {
+    const dato = req.body;
+
+    try {
+
+        const catalogo = await Catalogo.findById( req.params.id );
+        if ( !catalogo ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No se encontró el catálogo para actualizar'
+            });
+        }
+
+        const actualizar = await Catalogo.updateOne( { '_id': req.params.id }, { $set: { 'catalogoDatos.$[catDato].descripcion': dato.descripcion } }, { arrayFilters: [{ "catDato._id": dato.idActualizar }] }, { new: true} )
+        if ( actualizar.modifiedCount > 0 ) {
+
+            const datoActualizado = await Catalogo.findById( req.params.id );
+            res.status(200).json({
+                ok: true,
+                dato: datoActualizado
+            })
+        } else {
+            res.status(401).json({
+                ok: false,
+                msg: "No se pudo actualizar el dato del catálogo"
+            })
+        }
+        
+    } catch (error) {
+        console.log( error );
+        res.status(500).json({
+            ok: false,
+            msg: 'Problemas al actualizar la información del catálogo'
+        })
+    }
+}
+
+const eliminarDato = async ( req, res = response ) => {
+    const dato = req.body;
+
+    try {
+
+        const catalogo = await Catalogo.findById( dato.id );
+        if ( !catalogo ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No se encontró el catálogo para eliminar'
+            });
+        }
+
+        console.log({catalogo});
+
+        const datoEliminar = await Catalogo.updateOne( { _id : catalogo.id }, { $pull : { "catalogoDatos": { "_id": dato.idEliminar } } } );
+        
+        console.log({datoEliminar});
+        if ( datoEliminar.modifiedCount > 0 ) {
+            res.status(200).json({
+                ok: true,
+                dato: datoEliminar
+            })
+        } else {
+            res.status(401).json({
+                ok: false,
+                msg: "No se pudo eliminar el dato del catálogo"
+            })
+        }
+        
+    } catch (error) {
+        console.log( error );
+        res.status(500).json({
+            ok: false,
+            msg: 'Problemas al eliminar el catálogo'
         })
     }
 }
 
 module.exports = {
-    obtenerCatalogos,
-    agregarCatalogo,
+    actualizarDato,
     actualizarCatalogo,
+    agregarDato,
+    agregarCatalogo,
+    eliminarDato,
     eliminarCatalogo,
+    obtenerCatalogos,
 }
--- routes/catalogos.js
+++ routes/catalogos.js
@@ -7,12 +7,14 @@ const { check } = require('express-validator');
 
 const { fieldsValidate } = require('../middlewares/fields-validate');
 const { jwtValidate } = require('../middlewares/jwt-validate');
-const { agregarCatalogo, obtenerCatalogos, actualizarCatalogo, eliminarCatalogo } = require('../controllers/catalogos');
+const { agregarCatalogo, obtenerCatalogos, actualizarCatalogo, eliminarCatalogo, agregarDato, actualizarDato, eliminarDato } = require('../controllers/catalogos');
 
 const router = Router();
 
 // Todos tienen que pasar por la validación del JWT
 router.use( jwtValidate );
+
+// *************** CATALOGOS *************** //
 
 // Obtener catalogos
 router.get( '/', obtenerCatalogos );
@@ -38,6 +40,39 @@ router.put(
     actualizarCatalogo );
 
 // Eliminar catalogo
-router.delete( '/:id', eliminarCatalogo );
+// router.delete( '/:id', eliminarCatalogo );
+
+// *************** DATOS *************** //
+
+// Agregar datos a un catálogo
+router.post(
+    '/dato/nuevo',
+    [
+        check('id', 'El ID del catálogo es obligatorio').not().isEmpty(),
+        check('descripcion', 'La descripción del dato es obligatoria').not().isEmpty(),
+        fieldsValidate
+    ],
+    agregarDato);
+
+// Actualiza un dato de catalogoDatos en un catálogo
+router.put(
+    '/dato/:id',
+    [
+        check('id', 'El ID del catálogo es obligatorio').not().isEmpty(),
+        check('idActualizar', 'El ID del dato a actualizar es obligatorio').not().isEmpty(),
+        check('descripcion', 'La descripción del dato es obligatoria').not().isEmpty(),
+        fieldsValidate
+    ],
+    actualizarDato);
+
+// Elimina un dato de catalogoDatos en un catálogo
+router.delete(
+    '/dato/',
+    [
+        check('id', 'El ID del catálogo es obligatorio').not().isEmpty(),
+        check('idEliminar', 'El ID del dato a eliminar es obligatorio').not().isEmpty(),
+        fieldsValidate
+    ],
+    eliminarDato);
 
 module.exports = router;
